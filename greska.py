b0420666f74d296968db3e5de5c64d70448a94d5
Xe-135 <konstantin.peric@protonmail.com>
2025-06-04 11:16:22 +0200

    Update greska.py
    Fixed typos

--- greska.py
+++ greska.py
@@ -1,17 +1,9 @@
-# Python code with simple errors
-
 def calculate_average(numbers):
     total = 0
-    for num in numbers
+    for num in numbers: #Typo fixed
         total += num
     average = total / len(numbers)
-    return averge  # Typo in variable name
+    return average  # Typo fixed
 
-nums = [10, 20, 30, 40, 50
+nums = [10, 20, 30, 40, 50]
 print("The average is:", calculate_average(nums))
-
-# Errors:
-# 1. Missing colon (:) after for loop declaration
-# 2. Typo in return statement variable name ('averge' instead of 'average')
-# 3. Missing closing bracket in list definition
-# 4. Missing closing parenthesis in print function
